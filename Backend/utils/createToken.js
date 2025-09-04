6522d7caf9435566ca0b3d3dfa46d43232edd73f
NaveenVinayan <vinayannaveen@gmail.com>
2025-09-04 18:08:03 +0530

    jwt correction

--- Backend/utils/createToken.js
+++ Backend/utils/createToken.js
@@ -6,7 +6,7 @@ const createToken = (req,res,userID) =>{
     res.cookie('jwt',token,{
         httpOnly:true,
         secure:process.env.NODE_ENV !== 'development',
-        sameStite:'strict',
+        sameSite:'none',
         maxAge:30*24*60*60*1000
 
     })
