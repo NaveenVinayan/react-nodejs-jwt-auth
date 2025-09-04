f63fec697068c554ee8802653c057d5d852b29a1
NaveenVinayan <vinayannaveen@gmail.com>
2025-09-04 17:36:12 +0530

    backend deployed

--- frontend/src/api/adminService.js
+++ frontend/src/api/adminService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/";
+const API_URL = "https://react-nodejs-jwt-auth.onrender.com/";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
